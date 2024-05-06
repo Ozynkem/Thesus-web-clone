c08a1f1c16f1d22fb80aa0cfd3558aff3ecb5f8a
ozioma <Ozynkem01@gmail.com>
2024-05-06 13:11:26 -0700

    first commit

---
+++ Assets/vendors/carousel/js/app.js
@@ -0,0 +1,26 @@
+(function($){
+    "use strict";
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText:['<i class="fas fa-arrow-left"></i>','<i class="fas fa-arrow-right"></i>'],
+        autoplay:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+})
+ (jQuery)
+
+
+
